bc97dd88688808c900feaacceb8b2789797ac4e0
Riccardo Marchi <riccardo.marchi4@gmail.com>
2018-07-23 17:25:38 +0200

    add register or login activity

--- android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/MainActivity.java
+++ android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/MainActivity.java
@@ -1,13 +1,30 @@
 package com.mastercypher.university.mobile.datdog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnContinue = findViewById(R.id.button);
+
+        mBtnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegOrLogActivity.class));
+            }
+        });
     }
+
+
 }

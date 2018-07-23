a565d3916a95c37aa0825d5513800653471a32c8
AleRiccardi <alericcardi1996@gmail.com>
2018-07-24 00:15:58 +0200

    Added view New Report

---
+++ ios/Datdog/LostController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Datdog
+//
+//  Created by Alessandro Riccardi on 22/07/2018.
+//  Copyright © 2018 Mastercypher. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class LostController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+

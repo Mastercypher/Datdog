7fed07bb67117075f77d811b48ab1c5d88b7d05a
Riccardo Marchi <riccardo.marchi4@gmail.com>
2018-07-23 18:10:30 +0200

    add login activity

--- android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/RegOrLogActivity.java
+++ android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/RegOrLogActivity.java
@@ -1,13 +1,26 @@
 package com.mastercypher.university.mobile.datdog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegOrLogActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_or_log);
+        btnLogin = findViewById(R.id.button2);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegOrLogActivity.this, LoginActivity.class));
+            }
+        });
     }
 }

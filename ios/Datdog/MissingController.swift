81bc1d7685aa07a49df8ecf76785c29332a6ac38
AleRiccardi <alericcardi1996@gmail.com>
2018-07-23 22:51:41 +0200

    View background completed iphone 8 (not plus)

--- ios/Datdog/MissingController.swift
+++ ios/Datdog/MissingController.swift
@@ -11,13 +11,36 @@ import QuartzCore
 
 class MissingController: UIViewController {
     
-    @IBOutlet weak var btnMissed: UIButton!
+    @IBOutlet weak var viewReport: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
-        btnMissed.layer.cornerRadius = 0.5 * btnMissed.bounds.size.width;
-        btnMissed.imageEdgeInsets = UIEdgeInsetsMake(20, 20, 20, 20)
+        viewReport.layer.shadowColor = UIColor.lightGray.cgColor
+        viewReport.layer.shadowOpacity = 15
+        viewReport.layer.shadowOffset = CGSize(width: -2, height: 10)
+        viewReport.layer.shadowRadius = 10
+        
+        
+        
+        
+        
+        // screen width and height:
+        let width = viewReport.frame.size.width
+        let height = viewReport.frame.size.height
+        print(width, height)
+        let image = UIImage(named: "image_report")
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+
+        // you can change the content mode:
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10;
+        viewReport.addSubview(imageView)
+        viewReport.sendSubview(toBack: imageView)
+
+
     }
     
     override func didReceiveMemoryWarning() {

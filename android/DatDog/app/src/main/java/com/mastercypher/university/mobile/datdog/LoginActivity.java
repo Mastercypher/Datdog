8db8b438e5ada834f8782893e3836fe9b6944071
Riccardo Marchi <riccardo.marchi4@gmail.com>
2018-07-23 23:48:06 +0200

    add all five elements to navbar

--- android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/LoginActivity.java
+++ android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.mastercypher.university.mobile.datdog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.button4);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
     }
 }

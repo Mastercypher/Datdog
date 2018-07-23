56240d99e4741f5f3739efe4f813fe46f611741a
AleRiccardi <alericcardi1996@gmail.com>
2018-07-23 23:21:30 +0200

    Missing view terminated

--- ios/Datdog/MissingController.swift
+++ ios/Datdog/MissingController.swift
@@ -12,39 +12,47 @@ import QuartzCore
 class MissingController: UIViewController {
     
     @IBOutlet weak var viewReport: UIView!
+    @IBOutlet weak var viewLost: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         viewReport.layer.shadowColor = UIColor.lightGray.cgColor
         viewReport.layer.shadowOpacity = 15
         viewReport.layer.shadowOffset = CGSize(width: -2, height: 10)
         viewReport.layer.shadowRadius = 10
         
+        // Set the image for the REPORT view
+        let widthR = viewReport.frame.size.width
+        let heightR = viewReport.frame.size.height
+        let imgReport = UIImage(named: "image_dog_face")
+        let imvReport = UIImageView(image: imgReport!)
+        imvReport.frame = CGRect(x: 0, y: 0, width: widthR, height: heightR)
+        imvReport.contentMode = UIViewContentMode.scaleAspectFill
+        imvReport.clipsToBounds = true
+        imvReport.layer.cornerRadius = 10;
+        viewReport.addSubview(imvReport)
+        viewReport.sendSubview(toBack: imvReport)
         
+        // Set the image for the LOST view
+        let widthL = viewLost.frame.size.width
+        let heightL = viewLost.frame.size.height
+        let imgLost = UIImage(named: "image_dog_car")
+        let imvLost = UIImageView(image: imgLost!)
+        imvLost.frame = CGRect(x: 0, y: 0, width: widthL, height: heightL)
+        imvLost.contentMode = UIViewContentMode.scaleAspectFill
+        imvLost.clipsToBounds = true
+        imvLost.layer.cornerRadius = 10;
+        viewLost.addSubview(imvLost)
+        viewLost.sendSubview(toBack: imvLost)
         
-        
-        
-        // screen width and height:
-        let width = viewReport.frame.size.width
-        let height = viewReport.frame.size.height
-        print(width, height)
-        let image = UIImage(named: "image_report")
-        let imageView = UIImageView(image: image!)
-        imageView.frame = CGRect(x: 0, y: 0, width: width, height: height)
-
-        // you can change the content mode:
-        imageView.contentMode = UIViewContentMode.scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 10;
-        viewReport.addSubview(imageView)
-        viewReport.sendSubview(toBack: imageView)
-
-
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
 }
 

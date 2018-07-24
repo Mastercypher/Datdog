0e770c32809167b0fe11c2d03efba2adb47687af
AleRiccardi <alericcardi1996@gmail.com>
2018-07-24 16:02:57 +0200

    Added keyboard scroll diop.

--- ios/Datdog/RegistrationController.swift
+++ ios/Datdog/RegistrationController.swift
@@ -24,8 +24,6 @@ class RegistrationController : UIViewController, UITextFieldDelegate {
     @IBOutlet weak var constraintContentHeight: NSLayoutConstraint!
     
     var activeField: UITextField?
-    var lastOffset: CGPoint!
-    var keyboardHeight: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,47 +37,63 @@ class RegistrationController : UIViewController, UITextFieldDelegate {
         txfPassword.delegate = self
         txfRepeatPassword.delegate = self
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+
+    override func viewWillAppear(_ animated:Bool) {
+        super.viewWillAppear(animated)
+        
+        // Add this observers to observe keyboard shown and hidden events
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(aNotification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(aNotification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
     }
     
-    deinit {
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardDidShow, object: nil)
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardDidHide, object: nil)
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        //Remove the observers added for keyboard from your ViewController
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
     }
     
-    func hideKeyboard(){
-        txfName.resignFirstResponder()
-        txfSurname.resignFirstResponder()
-        txfPhone.resignFirstResponder()
-        txfBirth.resignFirstResponder()
-        txfEmail.resignFirstResponder()
-        txfPassword.resignFirstResponder()
-        txfRepeatPassword.resignFirstResponder()
-        txfRepeatPassword.resignFirstResponder()
-    }
-    
+    // – UITextField Delegates
+    // ViewController activeField is an object of UITextField which will be used to manage and resign current active textField
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        hideKeyboard()
+        textField.resignFirstResponder()
         return true
     }
     
-    @objc func keyboardWillChange(notification: Notification){
-        guard let keyboardReact = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-            return
-        }
-        print(keyboardReact.height)
-        if (notification.name == Notification.Name.UIKeyboardWillShow || notification.name == Notification.Name.UIKeyboardWillChangeFrame) {
-            view.frame.origin.y = -keyboardReact.height
-            print(keyboardReact.height)
-        } else {
-            view.frame.origin.y = 0
-            print(0)
-
-        }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeField = textField
     }
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        activeField = nil
+    }
     
+    // Called when the UIKeyboardWillHide is sent
+    // This method is called from selector. So it requires @objc keyword and this method will adjust your scrollView (here myScrollView)  and textFields to show as original.
+    
+    @objc func keyboardWillBeHidden(aNotification: NSNotification) {
+        let contentInsets: UIEdgeInsets = .zero
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        
+    }
+    
+    // Called when the UIKeyboardWillShow is sent
+    //This method will adjust your scrollView and will show textFields above the keyboard.
+    @objc func keyboardWillShow(aNotification: NSNotification) {
+        var info = aNotification.userInfo!
+        let kbSize: CGSize = ((info["UIKeyboardFrameEndUserInfoKey"] as? CGRect)?.size)!
+        let contentInsets: UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0)
+        var aRect: CGRect = self.view.frame
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        aRect.size.height -= kbSize.height
+        print(kbSize.height)
+        if (!aRect.contains(activeField!.frame.origin)) {
+            scrollView.scrollRectToVisible(activeField!.frame, animated: true)
+        }
+    }
 }

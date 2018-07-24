866652d8b02b8992586ab0ce1c7fca9c1c114ff8
Riccardo Marchi <riccardo.marchi4@gmail.com>
2018-07-24 10:31:14 +0200

    add activities related to navbar

--- android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/ConnectActivity.java
+++ android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/ConnectActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class ConnectActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -19,19 +19,19 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-
+                    startActivity(new Intent(ConnectActivity.this, HomeActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
                     return true;
                 case R.id.navigation_missing:
-                    startActivity(new Intent(HomeActivity.this, MissingActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    startActivity(new Intent(ConnectActivity.this, MissingActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
                     return true;
                 case R.id.navigation_friends:
-                    startActivity(new Intent(HomeActivity.this, FriendsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    startActivity(new Intent(ConnectActivity.this, FriendsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
                     return true;
                 case R.id.navigation_connect:
-                    startActivity(new Intent(HomeActivity.this, ConnectActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+
                     return true;
                 case R.id.navigation_dogs:
-                    startActivity(new Intent(HomeActivity.this, DogsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    startActivity(new Intent(ConnectActivity.this, DogsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
                     return true;
             }
             return false;
@@ -46,7 +46,7 @@ public class HomeActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        navigation.setSelectedItemId(R.id.navigation_home);
+        navigation.setSelectedItemId(R.id.navigation_connect);
     }
 
 }

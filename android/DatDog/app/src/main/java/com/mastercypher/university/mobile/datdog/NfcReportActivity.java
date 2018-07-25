3dc579912c7f39dd39e20063992627ac29f69e07
Riccardo Marchi <riccardo.marchi4@gmail.com>
2018-07-25 10:00:20 +0200

    add nfc report

---
+++ android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/NfcReportActivity.java
@@ -0,0 +1,52 @@
+package com.mastercypher.university.mobile.datdog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class NfcReportActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    startActivity(new Intent(NfcReportActivity.this, HomeActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    return true;
+                case R.id.navigation_missing:
+
+                    return true;
+                case R.id.navigation_friends:
+                    startActivity(new Intent(NfcReportActivity.this, FriendsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    return true;
+                case R.id.navigation_connect:
+                    startActivity(new Intent(NfcReportActivity.this, ConnectActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    return true;
+                case R.id.navigation_dogs:
+                    startActivity(new Intent(NfcReportActivity.this, DogsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_report);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_missing);
+    }
+
+}

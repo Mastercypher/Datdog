3ba1fa71fb21642751db8c4dc2bb940e9429be0d
Riccardo Marchi <riccardo.marchi4@gmail.com>
2018-07-25 15:06:16 +0200

    add navbar to those activities which didn't have it

--- android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/ConnectDogActivity.java
+++ android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/ConnectDogActivity.java
@@ -1,13 +1,51 @@
 package com.mastercypher.university.mobile.datdog;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ConnectDogActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    startActivity(new Intent(ConnectDogActivity.this, HomeActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    return true;
+                case R.id.navigation_missing:
+                    startActivity(new Intent(ConnectDogActivity.this, MissingActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    return true;
+                case R.id.navigation_friends:
+                    startActivity(new Intent(ConnectDogActivity.this, FriendsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    return true;
+                case R.id.navigation_connect:
+
+                    return true;
+                case R.id.navigation_dogs:
+                    startActivity(new Intent(ConnectDogActivity.this, DogsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_dog);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_connect);
     }
 }

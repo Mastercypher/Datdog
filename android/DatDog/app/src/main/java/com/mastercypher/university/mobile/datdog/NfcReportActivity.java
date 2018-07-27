70dd413a485b71aaf182b9928aeb6fa91e50639f
Riccardo Marchi <riccardo.marchi4@gmail.com>
2018-07-27 03:19:58 +0200

    add map usage

--- android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/NfcReportActivity.java
+++ android/DatDog/app/src/main/java/com/mastercypher/university/mobile/datdog/NfcReportActivity.java
@@ -1,14 +1,23 @@
 package com.mastercypher.university.mobile.datdog;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.DrawableContainer;
+import android.location.Location;
+import android.location.LocationManager;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,6 +35,7 @@ public class NfcReportActivity extends AppCompatActivity {
     private Switch swtLoc;
     private CheckBox cbPhone;
     private Button btnRepNfc;
+    private LocationManager locManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -74,7 +84,7 @@ public class NfcReportActivity extends AppCompatActivity {
         cbPhone.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
+                if (isChecked && !txtScan.getText().toString().equals("Scanning")) {
                     btnRepNfc.setBackgroundColor(Color.parseColor("#e4af09"));
                     btnRepNfc.setEnabled(true);
                 } else {
@@ -87,16 +97,79 @@ public class NfcReportActivity extends AppCompatActivity {
         btnRepNfc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(NfcReportActivity.this, ConfirmationReportActivity.class));
+                if (swtLoc.isChecked()) {
+                    locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    if (ActivityCompat.checkSelfPermission(NfcReportActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(NfcReportActivity.this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(NfcReportActivity.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+                    } else {
+                        Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        System.out.println(location.getLatitude() + " --- " + location.getLongitude());
+                        Intent it = new Intent(NfcReportActivity.this, ConfirmationReportActivity.class);
+                        it.putExtra("lat", location.getLatitude());
+                        it.putExtra("long", location.getLongitude());
+                        startActivity(it);
+                    }
+                } else {
+                    startActivity(new Intent(NfcReportActivity.this, ConfirmationReportActivity.class));
+                }
             }
         });
 
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //All's good, HE SAID YES!
+                    @SuppressLint("MissingPermission")
+                    Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    Intent it = new Intent(NfcReportActivity.this, ConfirmationReportActivity.class);
+                    it.putExtra("lat", location.getLatitude());
+                    it.putExtra("long", location.getLongitude());
+                    startActivity(it);
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(NfcReportActivity.this);
+                    builder.setTitle("GPS permission not granted");
+                    builder.setMessage("We will not use your location because you did not grant the permission.");
+                    builder.setCancelable(true);
+                    builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            startActivity(new Intent(NfcReportActivity.this, ConfirmationReportActivity.class));
+                        }
+                    });
+                    builder.show();
+                }
+            }
+        }
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (!nfcAdapter.isEnabled()) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(NfcReportActivity.this);
+            builder.setTitle("Enable NFC");
+            builder.setMessage("Your NFC is disabled. Enable it to make it work.");
+            builder.setCancelable(true);
+            builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+        }
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
                 new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
@@ -121,8 +194,11 @@ public class NfcReportActivity extends AppCompatActivity {
                 }
                 hexdump += x + ' ';
             }
-            Toast.makeText(getApplicationContext(),"NFC Tag serial number: " + hexdump, Toast.LENGTH_LONG).show();
-            txtScan.setText(hexdump);
+            txtScan.setText(hexdump.replace(" ", ""));
+            if (cbPhone.isChecked()) {
+                btnRepNfc.setBackgroundColor(Color.parseColor("#e4af09"));
+                btnRepNfc.setEnabled(true);
+            }
         }
     }
 }
